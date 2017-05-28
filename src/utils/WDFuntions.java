3815641be33ec8353224a4f83319e8b745f1df3d
Ehsanul Dodul <ehsanul.dodul@gmail.com>
2017-05-28 12:55:47 -0400

    Locators & Values

--- src/utils/WDFuntions.java
+++ src/utils/WDFuntions.java
@@ -1,17 +1,25 @@
 package utils;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class WDFuntions {
 
+	public static WebDriver driver;
+	
 	public void typeByXpath (String locators, String values){
-		
+		driver.findElement(By.xpath(locators)).clear();
+		driver.findElement(By.xpath(locators)).sendKeys(values);
 	};
 	
 	public void typeByName (String locators, String values){
-		
+		driver.findElement(By.name(locators)).clear();
+		driver.findElement(By.name(locators)).sendKeys(values);
 	};
 	
-	public void typeById (String locators, String values){
-		
+	public void typeById (String locators){
+		driver.findElement(By.id(locators)).clear();
+		driver.findElement(By.id(locators)).click();
 	};
 	
 }
