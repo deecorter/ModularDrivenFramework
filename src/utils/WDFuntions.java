eb7eb27d6105e8d96ac2aec74ca09416cfd9613c
Ehsanul Dodul <ehsanul.dodul@gmail.com>
2017-05-28 12:44:00 -0400

    Locators & Value

--- src/utils/WDFuntions.java
+++ src/utils/WDFuntions.java
@@ -2,4 +2,16 @@ package utils;
 
 public class WDFuntions {
 
+	public void typeByXpath (String locators, String values){
+		
+	};
+	
+	public void typeByName (String locators, String values){
+		
+	};
+	
+	public void typeById (String locators, String values){
+		
+	};
+	
 }
